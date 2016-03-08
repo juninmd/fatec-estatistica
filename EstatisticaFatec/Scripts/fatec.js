96d9527fae26dad7276cdd9ebfed0b4de38880da
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-03-07 22:10:22 -0300

    Trocando , por ;

--- EstatisticaFatec/Scripts/fatec.js
+++ EstatisticaFatec/Scripts/fatec.js
@@ -1,7 +1,58 @@
-﻿function formatarInput() {
-    var texto = document.getElementById('txtParametros');
-    while (texto.contains(/  /g)) {
-        texto = texto.replace(/  /g, " ");
+﻿function formatarInput(contador) {
+    if (contador == null)
+        contador = 0;
+
+    var texto = document.getElementById('txtParametros').value;
+
+    texto = texto.allTrim();
+
+    var pattern = /,,/g;
+    var myArray = texto.match(pattern);
+
+    while (myArray != null) {
+        texto = texto.replace(/,,/g, ",");
+        myArray = texto.match(pattern);
     }
-    return texto.replace(/ /g, ",");
+
+    pattern = /,/g;
+    myArray = texto.match(pattern);
+
+    while (myArray != null) {
+        texto = texto.replace(/,/g, ";");
+        myArray = texto.match(pattern);
+    }
+
+    pattern = /;;/g;
+    myArray = texto.match(pattern);
+
+    while (myArray != null) {
+        texto = texto.replace(/;;/g, ";");
+        myArray = texto.match(pattern);
+    }
+
+    pattern = / /g;
+    myArray = texto.match(pattern);
+
+    while (myArray != null) {
+        texto = texto.replace(/ /g, "");
+        myArray = texto.match(pattern);
+    }
+
+    if (contador == 0)
+        formatarInput(1);
+
+    document.getElementById('txtParametros').value = texto.replace(/ /g, ";");
 }
+String.prototype.allTrim = String.prototype.allTrim ||
+     function () {
+         return this.replace(/\s+/g, ' ')
+                    .replace(/^\s+|\s+$/, '');
+
+     };
+
+String.prototype.removeDuplicates = String.prototype.removeDuplicates ||
+     function () {
+         return this.replace(/;+/g, ' ')
+                    .replace(/^\s+|\s+$/, '');
+
+     };
