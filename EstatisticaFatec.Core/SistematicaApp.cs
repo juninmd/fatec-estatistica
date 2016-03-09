15a975899b9175f0e5c9e6a9fbc3360017a6c888
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-03-08 21:52:28 -0300

    Correção sistematica.

--- EstatisticaFatec.Core/SistematicaApp.cs
+++ EstatisticaFatec.Core/SistematicaApp.cs
@@ -8,19 +8,21 @@ namespace EstatisticaFatec.Core
     {
         public SistematicaEntity Build(int amostra, int inicial, int populacao)
         {
+            var inicicalOriginal = inicial;
             var repeticao = (int)Math.Round(populacao / (decimal)amostra);
-            var rolSistematica = new List<int> {inicial};
+            var rolSistematica = new List<int> {};
 
-            for (var i = 1; i < amostra; i++)
+            while (inicial < populacao)
             {
-                rolSistematica.Add(inicial + repeticao);
+                rolSistematica.Add(inicial);
+                inicial = inicial + repeticao;
             }
 
             return new SistematicaEntity
             {
                 Resultado = rolSistematica,
                 Amostra = amostra,
-                Inicial = inicial,
+                Inicial = inicicalOriginal,
                 Populacao = populacao
             };
         }
