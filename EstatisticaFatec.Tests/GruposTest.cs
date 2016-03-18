5c706628aafdc0f3ab9c749acea62a1c1a763fe3
Antonio <antonio@engsolutions.com.br>
2016-03-18 13:18:19 -0300

    classe de Testes - grupos

---
+++ EstatisticaFatec.Tests/GruposTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using EstatisticaFatec.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EstatisticaFatec.Tests
+{
+    [TestClass]
+    public class GruposTest
+    {
+        [TestMethod]
+        public void Input()
+        {
+            var inputs = new List<decimal>()
+            {
+              1,1,1,1,1,1,1,1,1,2,
+            };
+            var app = new GruposApp().Build(inputs);
+        }
+    }
+}
+
