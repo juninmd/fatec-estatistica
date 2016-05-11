db35aeaf211f2beb384d0178bafe97aad04a1d00
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-05-10 21:41:47 -0300

    criando classe variavel aleatoria

---
+++ EstatisticaFatec.Core/Models/VariavelAleatoria/VariavelAleatoriaEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace EstatisticaFatec.Core.Models.VariavelAleatoria
+{
+    public class VariavelAleatoriaEntity
+    {
+        public decimal Xi { get; set; }
+        public decimal Prob { get; set; }
+        public decimal Lucro { get; set; }
+        public decimal XIPI { get; set; }
+    }
+}
---
+++ EstatisticaFatec/Controllers/VariavelAleatoriaController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using EstatisticaFatec.Core;
+using EstatisticaFatec.Core.Models;
+using EstatisticaFatec.Core.Models.VariavelDiscreta;
+using EstatisticaFatec.Core.VariavelDiscreta;
+
+namespace EstatisticaFatec.Controllers
+{
+    public class VariavelAleatoriaController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(new VariavelDiscretaContainerEntity());
+        }
+
+        [HttpPost]
+        public ActionResult Index(InputEntity inputEntity)
+        {
+            var inputRequest = InputCore.Tratar(inputEntity);
+            if (inputRequest.IsError)
+            {
+                ModelState.AddModelError("error", inputRequest.Message);
+                return View(new VariavelDiscretaContainerEntity());
+            }
+
+            return View(new VariavelDiscretaApp().Build(inputRequest.Content));
+        }
+    }
+}
