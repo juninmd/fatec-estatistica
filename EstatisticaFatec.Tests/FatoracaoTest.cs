071b96383f957627c29439d9b124dace83f4025b
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-05-24 00:17:14 -0300

    teste de fatoração

---
+++ EstatisticaFatec.Tests/FatoracaoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using EstatisticaFatec.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EstatisticaFatec.Tests
+{
+    [TestClass]
+    public class FatoracaoTest
+    {
+        [TestMethod]
+        public void Input()
+        {
+            var fatorA = MathCoreApp.Fatorial(6);
+
+
+            var fatorB = MathCoreApp.Fatorial(1);
+            var fatorC = MathCoreApp.Fatorial(6 - 1);
+
+            var result = fatorA / (fatorB * fatorC);
+
+            var resultb = (result * Math.Pow(0.9, 1) * Math.Pow(0.1, 5)) * 100;
+        }
+    }
+}
+
